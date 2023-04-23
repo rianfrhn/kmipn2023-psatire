06444286cfcf5ad447633b9ce04844b0848538c9
rianfrhn <riandar.farhan1@gmail.com>
2023-04-23 18:56:31 +0700

    Buy stock ui & stock resource

---
+++ Resources/Stocks/StockResource.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name StockResource
+
+@export var stock_name : String
+@export var stock_desc : String
+@export var stock_cost : int = 0
+@export var arrival_days : int = 1
+@export var display_picture : Texture
