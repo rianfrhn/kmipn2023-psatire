bcdd2e59122c209c3ce0f062749377c46cf7e9e1
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2023-07-28 23:28:06 +0700

    add cutscene and pause button

--- Scenes/main_scene.gd
+++ Scenes/main_scene.gd
@@ -1,10 +1,17 @@
 extends Node3D
 
 @onready var light = $DirectionalLight3D
+@onready var btn_pause = $pause_btn
+@onready var pause_menu = preload("res://Scenes/User Interface/PauseMenu.tscn").instantiate()
+@onready var continue_btn: Button = pause_menu.get_node("btn_continue")
+@onready var exit_btn: Button = pause_menu.get_node("btn_exit")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Game.initialgame()
+	btn_pause.pressed.connect(show_pause_menu)
+	continue_btn.pressed.connect(hide_pause_menu)
+	exit_btn.pressed.connect(func(): SceneTransition.change_scene_path("res://Scenes/main_menu.tscn"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,3 +21,11 @@ func _process(delta):
 		light.rotation_degrees.x = 20 - ((Game.timehr-7 + Game.timemin/60.0)/(16.0-7)*4)
 	else:
 		light.rotation_degrees.x -= ((Game.timehr-7 + Game.timemin/60.0)/(16.0-7)*4)*delta
+
+func show_pause_menu() :
+	add_child(pause_menu)
+	pause_menu.show()
+
+func hide_pause_menu():
+	pause_menu.hide()
+	remove_child(pause_menu)
